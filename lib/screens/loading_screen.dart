433e22503140c9c99468e8d406014dae7005b9f6
Tanaka Kent <tanaken0304.0123@gmail.com>
2023-04-22 13:05:44 +0900

    permission check

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -7,7 +7,32 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  
+
   void getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    // raise error if location service is unavailable
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      // if the default permission is denied, request permission
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.low);
     print(position);
@@ -21,7 +46,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
           onPressed: () {
             getLocation();
           },
-          child: Text('Get Location'),
+          child: Text('Get Location!!!!'),
         ),
       ),
     );
